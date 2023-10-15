3eb0082c414b69fc91e607879e837eb01eec11c4
Tung <elrey0801@Tungs-MacBook-Air.local>
2023-10-15 19:51:28 +0700

    config on mac

--- models/user.js
+++ models/user.js
@@ -16,4 +16,5 @@ const userSchema = new Schema({
 });
 
 // module.exports = mongoose.model('User', userSchema);
-export default User = mongoose.model('User', userSchema);
+let User = mongoose.model('User', userSchema);
+export default User;
