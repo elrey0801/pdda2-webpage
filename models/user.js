427ca4d2a5846ca236f7b0c685382b1f7163dbf1
Tung <elrey0801@Tungs-MacBook-Air.local>
2023-10-24 10:46:10 +0700

    modify User model

--- models/user.js
+++ models/user.js
@@ -2,8 +2,11 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  _id: {
-    type: String
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
   },
   password: {
     type: String,
